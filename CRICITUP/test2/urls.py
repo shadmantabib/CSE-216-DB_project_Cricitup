a974d44a1071c45ee9f5aa94e73767b5b9b649b6
“shadmantabib” <shadmantabib2002@gmail.com>
2023-08-22 23:27:45 +0600

    git pull origin master

--- CRICITUP/test2/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='test2'
-urlpatterns = [
-    path('searchPlayer/', views.searchPlayer,name='searchPlayer')
-]
--- CRICITUP/testsql/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestsqlConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'testsql'
