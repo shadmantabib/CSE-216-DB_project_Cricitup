28c77354e19e7360ee06681ac5a8a9a57eed600e
“shadmantabib” <shadmantabib2002@gmail.com>
2023-09-07 22:48:40 +0600

    dumb i am

--- CRICITUP/addplayer/views.py
+++ CRICITUP/addplayer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.db import connection
 
-# Create your views here.
+
 
 def addplayer(request):
     if request.method == 'GET':
@@ -18,14 +18,14 @@ def addplayer(request):
         dob = request.POST['phone_number']
         file = request.FILES['image']
 
-        # Retrieve Team_id based on nationality (assuming Team_Name corresponds to nationality)
+       
         cursor = connection.cursor()
         cursor.execute("SELECT Team_id FROM Team WHERE Team_Name = %s", (nationality,))
         team_id = cursor.fetchone()[0]
 
         handle_uploaded_file(file)
 
-        # Fetch maximum personId from Person table
+     
         cursor.execute("SELECT MAX(personId) FROM Person")
         max_person_id = cursor.fetchone()[0]
         new_person_id = max_person_id + 1
@@ -35,14 +35,13 @@ def addplayer(request):
         max_stat_id = cursor.fetchone()[0]
         new_stat_id = max_stat_id + 1
 
-        # Insert data into Person table
+    
         cursor.execute(
             "INSERT INTO Person (personId, first_name, last_name, nationality, date_of_birth, Image_URL) "
             "VALUES (%s, %s, %s, %s, TO_DATE(%s, 'YYYY-MM-DD'), %s)",
             (new_person_id, fname, lname, nationality, dob, file.name)
         )
 
-        # Insert data into Player table
         cursor.execute(
             "INSERT INTO Player (playerId, type, Team_id, statId) "
             "VALUES (%s, %s, %s, %s)",
--- CRICITUP/umpires/views.py
+++ CRICITUP/umpires/views.py
@@ -62,15 +62,131 @@ def umpires_details(request, player_id):
 
     cursor.execute(sql_query_2,[player_id])
     player_stat=cursor.fetchone()
+    first_team_batting=[]
+    first_team_bowling=[]
+    # Define the SQL query using placeholders for parameters
+    query = """
+    SELECT 
+    (SELECT MU.MATCH_ID FROM MATCH_UMPIRE MU WHERE MU.umpire_id = %s) AS MATCH_ID,
+    (SELECT TEAM_NAME FROM TEAM T WHERE T.TEAM_ID = M.TEAM1_ID) || ' vs ' || (SELECT TEAM_NAME FROM TEAM T WHERE T.TEAM_ID = M.TEAM2_ID) || ' -' || (SELECT MU.MATCH_ID FROM MATCH_UMPIRE MU WHERE MU.umpire_id = %s) AS NAME,
+    M.MAN_OF_THE_MATCH,
+    M.WEATHER,
+    M.WINNER,
+    (SELECT ADDRESS FROM VENUE V WHERE V.VENUE_ID = M.VENUE_ID) AS GROUND
+    FROM MATCH M
+    JOIN MATCH_Umpire MU ON MU.MATCH_ID = M.MATCH_ID
+    WHERE MU.umpire_id = %s;
+
     
+    """
+    query2="""SELECT PLAYER_ID,(SELECT (FIRST_NAME||' '||LAST_NAME) FROM PERSON PR WHERE PR.PERSONID=S.PLAYER_ID) NAME , TOTAL_RUNS AS RUN ,TOTAL_BALLS_FACED AS BALL,TOTAL_SIXES_HIT AS SIX,TOTAL_FOURS_HIT AS FOUR  FROM SCORECARD S
+	WHERE match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s )
+	AND player_id IN (	SELECT PLAYERID player_id from player where team_id =(	SELECT team1_id from match
+	where match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s))
+	ORDER by player_id
+"""
+    query3="""
+ SELECT (SELECT team_name from team t where t.team_id=m.team1_id) name   from match m
+	where match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s)
+"""
+    query4="""
+	SELECT PLAYER_ID,(SELECT (FIRST_NAME||' '||LAST_NAME) FROM PERSON PR WHERE PR.PERSONID=S.PLAYER_ID) NAME , OVERS_BOWLED AS OVER ,TAKEN_WICKETS AS WICKET,GIVEN_TOTAL_RUNS AS RUN,MAIDEN_OVERS_GIVEN AS MAIDEN  FROM SCORECARD S
+	WHERE match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s)
+	AND player_id IN (	SELECT PLAYERID player_id from player where team_id =(	SELECT team1_id from match
+	where match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id%s)))
+	AND OVERS_BOWLED>0
+	ORDER by player_id
+	
+"""
+    query5="""
+SELECT PLAYER_ID,(SELECT (FIRST_NAME||' '||LAST_NAME) FROM PERSON PR WHERE PR.PERSONID=S.PLAYER_ID) NAME , TOTAL_RUNS AS RUN ,TOTAL_BALLS_FACED AS BALL,TOTAL_SIXES_HIT AS SIX,TOTAL_FOURS_HIT AS FOUR  FROM SCORECARD S
+	WHERE match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s)
+	AND player_id IN (	SELECT PLAYERID player_id from player where team_id =(	SELECT team2_id from match
+	where match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s)))
+	ORDER by player_id
+"""
+    query6="""
+	SELECT PLAYER_ID,(SELECT (FIRST_NAME||' '||LAST_NAME) FROM PERSON PR WHERE PR.PERSONID=S.PLAYER_ID) NAME , OVERS_BOWLED AS OVER ,TAKEN_WICKETS AS WICKET,GIVEN_TOTAL_RUNS AS RUN,MAIDEN_OVERS_GIVEN AS MAIDEN  FROM SCORECARD S
+	WHERE match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s)
+	AND player_id IN (	SELECT PLAYERID player_id from player where team_id =(	SELECT team2_id from match
+	where match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s)))
+	AND OVERS_BOWLED>0
+	ORDER by player_id
+	
+"""    
+    query7="""
+ SELECT (SELECT team_name from team t where t.team_id=m.team2_id) name   from match m
+	where match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s)
+"""
+    query8="""
+SELECT SUM(TOTAL_RUNS) AS RUN  FROM SCORECARD S
+	WHERE match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE PERSONID=%s)
+	AND player_id IN (	SELECT PLAYERID player_id from player where team_id =(	SELECT team1_id from match
+	where match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s)))
+	ORDER by player_id
+"""
+    query9="""
+SELECT SUM(TOTAL_RUNS) AS RUN  FROM SCORECARD S
+	WHERE match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE PERSONID=%s)
+	AND player_id IN (	SELECT PLAYERID player_id from player where team_id =(	SELECT team2_id from match
+	where match_id=(SELECT MATCH_ID FROM MATCH_UMPIRE WHERE umpire_id=%s)))
+	ORDER by player_id
+"""
+
+    # Execute the PL/SQL query to fetch team details
+    select_match_id_query = "SELECT match_id FROM match_umpire WHERE UMPIRE_ID = %s"
+
+    cursor.execute(select_match_id_query, (player_id,))
+
+    match_id = cursor.fetchone()[0]
+    with connection.cursor() as cursor:
+        cursor.execute(query, [player_id])
+        match = cursor.fetchone()  # Fetch the result
+        match_winner=cursor.callfunc('Find_Match_Winner',str,[match_id])
+        highest_scorer=cursor.callfunc('Find_Highest_Scorer',str,[match_id])
+
+        cursor.execute(query2,[match_id,match_id])
+        first_team_batting=cursor.fetchall()
+        cursor.execute(query3, [match_id])
+        first_team_name=cursor.fetchone()[0]
+        cursor.execute(query4,[match_id,match_id])
+        first_team_bowling=cursor.fetchall()
+        cursor.execute(query5,[match_id,match_id])
+        second_team_batting=cursor.fetchall()
+        cursor.execute(query7, [match_id])
+        second_team_name=cursor.fetchone()[0]
+        cursor.execute(query6,[match_id,match_id])
+        second_team_bowling=cursor.fetchall()
+        cursor.execute(query8,[match_id,match_id])
+        first_team_score=cursor.fetchone()[0]
+        cursor.execute(query9,[match_id,match_id])
+        second_team_score=cursor.fetchone()[0]
+
+
+
+
+
+
+
     context = {
         'player_id': player_id,
         'player_name': player_name,
         'player_nationality': player_nationality,
         'player_age': player_age,
         'player_image_url': player_image_url,
-        'player_stat':player_stat
-
+        'player_stat':player_stat,
+        'match': match,
+        'match_winner':match_winner,
+        'highest_scorer':highest_scorer,
+        'first_team_batting':first_team_batting,
+        'first_team_name':first_team_name,
+        'first_team_bowling':first_team_bowling,
+        'second_team_name':second_team_name,
+        'second_team_batting':second_team_batting,
+        'second_team_bowling':second_team_bowling,
+        'first_team_score':first_team_score,
+        'second_team_score':second_team_score
     }
+    
     
     return render(request, 'umpires/umpires_details.html', context)
