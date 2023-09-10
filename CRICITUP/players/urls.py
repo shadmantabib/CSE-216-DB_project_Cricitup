bd522f38b72a5f971d874bc40e995067e7214c70
Munim <munimthahmid2@gmail.com>
2023-09-10 23:11:28 +0600

    project final draft 1

--- CRICITUP/players/urls.py
+++ CRICITUP/players/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('players/<int:player_id>/', views.players_details, name='players_details'),
     path('show-all-players/',views.show_all_players,name='show_all_players'),
     path('player-batting-rank/',views.get_players_by_batting_rank,name='get_players_by_batting_rank'),
-    path('player-bowling-rank/',views.get_players_by_bowling_rank,name='get_players_by_bowling_rank')
+    path('player-bowling-rank/',views.get_players_by_bowling_rank,name='get_players_by_bowling_rank'),
+    path('searchPlayer/',views.searchPlayer,name='searchPlayer')
+
 
 ]
